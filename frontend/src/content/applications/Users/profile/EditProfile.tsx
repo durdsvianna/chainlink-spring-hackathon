ecc700cf430e564bf1b1e259835635acaed99a87
heronlancellot <heronlancellot@id.uff.br>
2023-05-13 23:48:28 -0300

    Feat: Add StyledImports to change the styled in cards in User Folder

--- frontend/src/content/applications/Users/profile/EditProfile.tsx
+++ frontend/src/content/applications/Users/profile/EditProfile.tsx
@@ -1,151 +1,91 @@
+import { useState } from 'react';
+import { Grid, Button, Avatar, IconButton, CardMedia } from '@mui/material';
 import {
-    Grid,
-    Card,
-    Box,
-    Button,
-    Avatar,
-    IconButton,
-    CardMedia
-  } from '@mui/material';
-  import { styled } from '@mui/material/styles';
-  import UploadTwoToneIcon from '@mui/icons-material/UploadTwoTone';
-  import { useState } from 'react';
+  AvatarWrapper,
+  ButtonUploadWrapper,
+  CardCover,
+  CardCoverAction,
+  Input
+} from './StyledImports';
+import UploadTwoToneIcon from '@mui/icons-material/UploadTwoTone';
 import UserEditPersonalDetails from 'src/components/User/UserEditPersonalDetails';
-  
-  function EditProfileTab({ user: UserProfile }) {
 
-    const [cover, setCover] = useState(UserProfile.coverImg);
-    const [avatar, setAvatar] = useState(UserProfile.avatar);
+function EditProfileTab({ user: UserProfile }) {
+  const [cover, setCover] = useState(UserProfile.coverImg);
+  const [avatar, setAvatar] = useState(UserProfile.avatar);
 
-    const Input = styled('input')({
-        display: 'none'
-      });
+  const handleChangeCover = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
 
-    const AvatarWrapper = styled(Card)(
-        ({ theme }) => `
-      
-          position: relative;
-          overflow: visible;
-          display: inline-block;
-          margin-top: -${theme.spacing(9)};
-          margin-left: ${theme.spacing(2)};
-      
-          .MuiAvatar-root {
-            width: ${theme.spacing(16)};
-            height: ${theme.spacing(16)};
-          }
-      `
-      );
-      
-    const ButtonUploadWrapper = styled(Box)(
-        ({ theme }) => `
-          position: absolute;
-          width: ${theme.spacing(4)};
-          height: ${theme.spacing(4)};
-          bottom: -${theme.spacing(1)};
-          right: -${theme.spacing(1)};
-      
-          .MuiIconButton-root {
-            border-radius: 100%;
-            background: ${theme.colors.primary.main};
-            color: ${theme.palette.primary.contrastText};
-            box-shadow: ${theme.colors.shadows.primary};
-            width: ${theme.spacing(4)};
-            height: ${theme.spacing(4)};
-            padding: 0;
-        
-            &:hover {
-              background: ${theme.colors.primary.dark};
-            }
-          }
-      `
-      );
-      
-    const handleChangeCover = (event) => {
-        const file = event.target.files[0];
-        const reader = new FileReader();
-        
-        reader.onloadend = () => {
-            setCover(reader.result);
-        };
-        
-        if (file) {
-            reader.readAsDataURL(file);
-        }
+    reader.onloadend = () => {
+      setCover(reader.result);
     };
 
-    const handleChangeAvatar = (event) => {
-        const file = event.target.files[0];
-        const reader = new FileReader();
-
-        reader.onloadend = () => {
-            setAvatar(reader.result);
-        }
-
-        if (file) {
-            reader.readAsDataURL(file);
-        }
+    if (file) {
+      reader.readAsDataURL(file);
     }
-    
-    const CardCover = styled(Card)(
-        ({ theme }) => `
-            position: relative;
-        
-            .MuiCardMedia-root {
-            height: ${theme.spacing(26)};
-            }
-        `
-    );
-    
-    const CardCoverAction = styled(Box)(
-        ({ theme }) => `
-            position: absolute;
-            right: ${theme.spacing(2)};
-            bottom: ${theme.spacing(2)};
-        `
-    );
-    
-    return (
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-      <CardCover>
-        <CardMedia image={cover} />
-        <CardCoverAction>
-          <Input accept="image/*" id="change-cover" multiple type="file" onChange={handleChangeCover}/>
-          <label htmlFor="change-cover">
-            <Button
-              startIcon={<UploadTwoToneIcon />}
-              variant="contained"
-              component="span"
+  };
+
+  const handleChangeAvatar = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setAvatar(reader.result);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <CardCover>
+          <CardMedia image={cover} />
+          <CardCoverAction>
+            <Input
+              accept="image/*"
+              id="change-cover"
+              multiple
+              type="file"
               onChange={handleChangeCover}
-            >
-              Modificar papel de parede
-            </Button>
-          </label>
-        </CardCoverAction>
-      </CardCover>
-      <AvatarWrapper>
-        <Avatar variant="rounded" alt={UserProfile.name} src={avatar} />
-        <ButtonUploadWrapper>
-          <Input
-            accept="image/*"
-            id="icon-button-file"
-            name="icon-button-file"
-            type="file"
-            onChange={handleChangeAvatar}
-          />
-          <label htmlFor="icon-button-file">
-            <IconButton component="span" color="primary">
-              <UploadTwoToneIcon />
-            </IconButton>
-          </label>
-        </ButtonUploadWrapper>
-      </AvatarWrapper>
+            />
+            <label htmlFor="change-cover">
+              <Button
+                startIcon={<UploadTwoToneIcon />}
+                variant="contained"
+                component="span"
+                onChange={handleChangeCover}
+              >
+                Modificar papel de parede
+              </Button>
+            </label>
+          </CardCoverAction>
+        </CardCover>
+        <AvatarWrapper>
+          <Avatar variant="rounded" alt={UserProfile.name} src={avatar} />
+          <ButtonUploadWrapper>
+            <Input
+              accept="image/*"
+              id="icon-button-file"
+              name="icon-button-file"
+              type="file"
+              onChange={handleChangeAvatar}
+            />
+            <label htmlFor="icon-button-file">
+              <IconButton component="span" color="primary">
+                <UploadTwoToneIcon />
+              </IconButton>
+            </label>
+          </ButtonUploadWrapper>
+        </AvatarWrapper>
       </Grid>
-        <UserEditPersonalDetails user={UserProfile} /> 
-      </Grid>
-    );
-  }
-  
-  export default EditProfileTab;
-  
+      <UserEditPersonalDetails user={UserProfile} />
+    </Grid>
+  );
+}
+
+export default EditProfileTab;
--- frontend/src/content/applications/Users/profile/ProfileCover.tsx
+++ frontend/src/content/applications/Users/profile/ProfileCover.tsx
@@ -2,56 +2,31 @@ import PropTypes from 'prop-types';
 import {
   Box,
   Typography,
-  Card,
   Tooltip,
   Avatar,
   CardMedia,
-  IconButton,
+  IconButton
 } from '@mui/material';
-import { styled } from '@mui/material/styles';
-
+import { AvatarWrapper, CardCover } from './StyledImports';
 import ArrowBackTwoToneIcon from '@mui/icons-material/ArrowBackTwoTone';
 import MoreHorizTwoToneIcon from '@mui/icons-material/MoreHorizTwoTone';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import InstagramIcon from '@mui/icons-material/Instagram';
 
-const AvatarWrapper = styled(Card)(
-  ({ theme }) => `
-
-    position: relative;
-    overflow: visible;
-    display: inline-block;
-    margin-top: -${theme.spacing(9)};
-    margin-left: ${theme.spacing(2)};
-
-    .MuiAvatar-root {
-      width: ${theme.spacing(16)};
-      height: ${theme.spacing(16)};
-    }
-`
-);
-
-const CardCover = styled(Card)(
-  ({ theme }) => `
-    position: relative;
-
-    .MuiCardMedia-root {
-      height: ${theme.spacing(26)};
-    }
-`
-);
-
 const ProfileCover = ({ user: UserProfile }) => {
-
   const handleButtonHome = () => {
-    window.location.href = "/dapp/#";
+    window.location.href = '/dapp/#';
   };
 
   return (
     <>
       <Box display="flex" mb={3}>
         <Tooltip arrow placement="top" title="Go back">
-          <IconButton color="primary" sx={{ p: 2, mr: 2 }} onClick={handleButtonHome}>
+          <IconButton
+            color="primary"
+            sx={{ p: 2, mr: 2 }}
+            onClick={handleButtonHome}
+          >
             <ArrowBackTwoToneIcon />
           </IconButton>
         </Tooltip>
@@ -64,12 +39,16 @@ const ProfileCover = ({ user: UserProfile }) => {
           </Typography>
         </Box>
       </Box>
-      
+
       <CardCover>
         <CardMedia image={UserProfile.coverImg} />
       </CardCover>
       <AvatarWrapper>
-        <Avatar variant="rounded" alt={UserProfile.name} src={UserProfile.avatar} />
+        <Avatar
+          variant="rounded"
+          alt={UserProfile.name}
+          src={UserProfile.avatar}
+        />
       </AvatarWrapper>
 
       <Box py={2} pl={2} mb={3}>
@@ -86,12 +65,12 @@ const ProfileCover = ({ user: UserProfile }) => {
           justifyContent="space-between"
         >
           <Box>
-              <IconButton>
-                <GitHubIcon/>
-              </IconButton>
+            <IconButton>
+              <GitHubIcon />
+            </IconButton>
 
             <IconButton>
-              <InstagramIcon/>
+              <InstagramIcon />
             </IconButton>
 
             <IconButton color="primary" sx={{ p: 0.5 }}>
